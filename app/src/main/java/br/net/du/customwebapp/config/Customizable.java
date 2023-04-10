b8bc8f0c354848210387579d8e4237e74b0c1a85
Eduardo Sztokbant <sztokbant@posteo.net>
2023-04-10 10:43:42 -0700

    First steps towards customizing for sztoks.com

--- app/src/main/java/br/net/du/customwebapp/config/Customizable.java
+++ app/src/main/java/br/net/du/customwebapp/config/Customizable.java
@@ -6,16 +6,19 @@ import br.net.du.customwebapp.model.ButtonConfig;
 public class Customizable {
     // Define buttons in floating action menu and their paths
     public static final ButtonConfig[] BUTTON_CONFIGS = {
-        new ButtonConfig("CustomWebApp", R.drawable.ic_launcher, "sztokbant/CustomWebApp-Android")
+        new ButtonConfig(
+                "CustomWebApp",
+                R.drawable.ic_launcher,
+                "https://github.com/sztokbant/CustomWebApp-Android")
     };
 
     // Define other domains allowed in the app's webview
-    public static final String[] OTHER_ALLOWED_DOMAINS = new String[] {};
+    public static final String[] OTHER_ALLOWED_DOMAINS = new String[] {"sztoks.com"};
 
     // Define signed-out URL paths to prevent floating action menu from being displayed
-    public static final String[] SIGNED_OUT_URL_PATTERNS = new String[] {};
+    public static final String[] SIGNED_OUT_URL_PATTERNS = new String[] {"/login", "/signup"};
 
     // Define optional prefix, suffix for extra allowed domains
-    public static final String GENERIC_DOMAIN_PREFIX = null;
-    public static final String GENERIC_DOMAIN_SUFFIX = null;
+    public static final String GENERIC_DOMAIN_PREFIX = "sztoks-";
+    public static final String GENERIC_DOMAIN_SUFFIX = ".herokuapp.com";
 }
